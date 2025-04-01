bc48eb534cf8f68eded92a0499bff89e02e9cd3d
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-04-01 22:38:33 +0000

    last move was updating it. next steps include adding better visuals for graphs

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,35 +1,35 @@
 class NotesController < ApplicationController
   #display the homepage
   def homepage
-    require "http"
-    require "json"
-    require "dotenv/load"
+    # require "http"
+    # require "json"
+    # require "dotenv/load"
 
-    request_headers_hash = {
-      "Authorization" => "Bearer #{ENV.fetch("OPENAI_API_KEY")}",
-      "content-type" => "application/json"
-    }
-    request_body_hash = {
-      "model" => "gpt-3.5-turbo",
-      "messages" => [
-        {
-          "role" => "system",
-          "content" => "You only respond giving numbered headlines. Do not add an introduction or a conclusion, just the information. Before each headline and number, give this exact set of symbols: '$%'. with each news headline, include the date from which you pulled this information. pull information from this month, march 2025"
-        },
-        {
-          "role" => "user",
-          "content" => "Give me the 10 top headlines from recent news. only relevant to the economy, politics, or business. "
-        }
-      ]
-    }
-    request_body_json = JSON.generate(request_body_hash)
-    raw_response = HTTP.headers(request_headers_hash).post(
-      "https://api.openai.com/v1/chat/completions",
-      :body => request_body_json
-    ).to_s
-    parsed_response = JSON.parse(raw_response)
-    news = parsed_response['choices'][0]['message']['content']
-    @items = news.split("$%").reject(&:empty?)
+    # request_headers_hash = {
+    #   "Authorization" => "Bearer #{ENV.fetch("OPENAI_API_KEY")}",
+    #   "content-type" => "application/json"
+    # }
+    # request_body_hash = {
+    #   "model" => "gpt-3.5-turbo",
+    #   "messages" => [
+    #     {
+    #       "role" => "system",
+    #       "content" => "You only respond giving numbered headlines. Do not add an introduction or a conclusion, just the information. Before each headline and number, give this exact set of symbols: '$%'. with each news headline, include the date from which you pulled this information. pull information from this month, march 2025"
+    #     },
+    #     {
+    #       "role" => "user",
+    #       "content" => "Give me the 10 top headlines from recent news. only relevant to the economy, politics, or business. "
+    #     }
+    #   ]
+    # }
+    # request_body_json = JSON.generate(request_body_hash)
+    # raw_response = HTTP.headers(request_headers_hash).post(
+    #   "https://api.openai.com/v1/chat/completions",
+    #   :body => request_body_json
+    # ).to_s
+    # parsed_response = JSON.parse(raw_response)
+    # news = parsed_response['choices'][0]['message']['content']
+    # @items = news.split("$%").reject(&:empty?)
 
     @notes = Note.all
 
